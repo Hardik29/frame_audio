2ba558087e5832200f4d91085fea540527fa82ef
Hardik29 <singhhardik14@gmail.com>
2024-02-05 23:54:30 +0530

    hy

--- app/api/route.js
+++ app/api/route.js
@@ -5,11 +5,14 @@ async function getResponse(req) {
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
   <meta property="fc:frame:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSshocBQggTnHIM40eyiP8EhAPhw8nQkYG9uQ&usqp=CAU" /> 
+  </head>
+  <body>
   <audio controls>
   <source src="https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF" type="audio/mp3">
   Your browser does not support the audio element.
   </audio>
-  </head></html> `);
+  </body>
+  </html> `);
 
 }
 
