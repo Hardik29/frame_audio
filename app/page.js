7e99848356d015df6f590685d48fe11ce29b98b3
Hardik29 <singhhardik14@gmail.com>
2024-02-06 00:38:27 +0530

    test3

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ const frameMetadata = getFrameMetadata({
 
   ],
   image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--hhyrvv9a--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/eefd4ew38ryyrfk6m5nk.png',
-  post_url: 'https://meme-frame-six.vercel.app/api',
+  post_url: 'https://unique-griffin-7a9646.netlify.app/api',
 });
 
 export const metadata = {
