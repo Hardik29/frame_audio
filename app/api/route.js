44d0517f49827bc6ffb69eec8492de730487b5cb
Hardik29 <singhhardik14@gmail.com>
2024-02-06 01:39:02 +0530

    test8

--- app/api/route.js
+++ app/api/route.js
@@ -5,9 +5,6 @@ async function getResponse(req) {
   return new NextResponse(`<!DOCTYPE html>
   <html lang="en">
   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmVqZ2VnY2d3NjQyM211cGJoejd5bDNtYmszcDBjeTl2eDMyanZ4YyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/TWsw90Csti2KCjvKfU/giphy.gif" />
       <meta property="fc:frame:button:1" content="To Podcast" />
