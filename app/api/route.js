0524f47ec4651400bc3ce51a3f13a3e446a1e107
Hardik29 <singhhardik14@gmail.com>
2024-02-06 00:24:56 +0530

    try1

--- app/api/route.js
+++ app/api/route.js
@@ -1,11 +1,20 @@
-import {  NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 async function getResponse(req) {
   console.log('API frame route called');
   return new NextResponse(`  <!DOCTYPE html><html><head> 
   <meta property="fc:frame" content="vNext" /> 
   <meta property="fc:frame:image" content="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmVqZ2VnY2d3NjQyM211cGJoejd5bDNtYmszcDBjeTl2eDMyanZ4YyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/TWsw90Csti2KCjvKfU/giphy.gif" /> 
-  </head></html> `);
+  </head>
+  <body>
+  <script>
+  document.addEventListener('DOMContentLoaded', function () {
+  var audio = new Audio("https://cyan-deep-moth-632.mypinata.cloud/ipfs/QmeAU8s7mh2BaZM46hCNKHTxPhWHNpje6BiJuuSwpLQAwF");
+  audio.play();
+  });
+  </script>
+  </body>
+  </html> `);
 
 }
 
