be3cfab6059eb06608242e178c6d27cf90f9bb2d
Hardik29 <singhhardik14@gmail.com>
2024-02-06 17:09:21 +0530

    test16

--- app/api/route.js
+++ app/api/route.js
@@ -2,14 +2,12 @@ import { NextResponse } from 'next/server';
 
 async function getResponse(req) {
   console.log('API frame route called');
-  return new NextResponse(`<!DOCTYPE html><html>
- <head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://www.canva.com/design/DAF73FvKU1U/k57xc8VQF31MlJ82-lx63g/view?utm_content=DAF73FvKU1U&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel" />
-  </head>
-  </html>
+  return new NextResponse(`  <!DOCTYPE html><html><head> 
+  <meta property="fc:frame" content="vNext" /> 
+  <meta property="fc:frame:image" content="https://www.canva.com/design/DAF73FvKU1U/k57xc8VQF31MlJ82-lx63g/view?utm_content=DAF73FvKU1U&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel" /> 
+  <meta property="fc:frame:button:1" content="To the podcast" /> 
+  </head></html> 
   `);
-
 } 
 
 export async function POST(req) {
